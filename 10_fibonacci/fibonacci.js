1e6591488891a57e6f617001804ed3a23c08b096
john <jfitzgerald1126@gmail.com>
2023-08-24 14:26:27 -0400

    solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 1 || num == 2) {
+        return 1;
+    } else if (num < 1) {
+        return 'OOPS';
+    } else {
+        let x = 1, y = 1;
+        let curr = 0;
+        for (let i = 2; i < num; i++) {
+            curr = x + y;
+            y = x;
+            x = curr;
+        }   
+        return curr;
+    }
 };
 
 // Do not edit below this line
