49b09fbc4e3b3d89c2b369f88b82b138ebe3d8d2
john <jfitzgerald1126@gmail.com>
2023-08-24 14:17:58 -0400

    solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    // remove punctuation
+    const processedString = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
