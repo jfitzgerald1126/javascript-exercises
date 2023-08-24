1b0c50d81244be99884a7adc9ce04e1e4162a46e
john <jfitzgerald1126@gmail.com>
2023-08-24 14:39:10 -0400

    Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function calcAge(person) {
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    }
 
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => (calcAge(oldest)) > (calcAge(curr)) ? oldest: curr);
 };
 
 // Do not edit below this line
